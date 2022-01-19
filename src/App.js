53596b3a93d717696eff89ec4b5b6d66f08e215c
Shashank nayak <shashanknayak.1209@gmail.com>
2022-01-19 23:55:38 +0530

    deleted some componenet and changed some routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies'
 import NotFound from './components/notFound';
-import Customer from './components/customer';
-import Rental from './components/rental'
+
 import Navbar from './components/navbar';
 import MoiveForm from './components/movieForm';
-import LoginForm from './components/loginform';
-import RegisterForm from './components/registerform'
+
 import MovieForm from './components/movieForm';
 import './App.css';
 
@@ -21,14 +19,12 @@ class App extends React.Component {
         <main className='container'>
           <Switch>
             <Route path='/newmovieform' component={MovieForm} />
-            <Route path='/register' component={RegisterForm} />
-            <Route path='/login' component={LoginForm} />
+
             <Route path='/movies/:id' component={MoiveForm} />
             <Route path='/movies' component={Movies} />
-            <Route path='/customer' component={Customer} />
-            <Route path='/rental' component={Rental} />
+
             <Route path='/not-found' component={NotFound} />
-            <Redirect from='/' exact to='/movies' />
+            <Route exact path='/' component={Movies} />
             <Redirect to='/not-found' />
           </Switch>
         </main>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import Movies from './movies';
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">
-                    Vidly
+                    MoviesList
                 </Link>
                 <button
                     className="navbar-toggler"
@@ -20,14 +21,14 @@ const Navbar = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item">
+                        {/* <li className="nav-item">
                             <NavLink
                                 className="nav-link"
                                 aria-current="page"
                                 to="/movies">
                                 Movies
                             </NavLink>
-                        </li>
+                        </li> */}
                         {/* <li className="nav-item">
                             <NavLink
                                 className="nav-link"
